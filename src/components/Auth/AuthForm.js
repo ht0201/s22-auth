b7f92bd83080932f9d4f2755110272138a2c6ed4
thai.vo <43738511+ht0201@users.noreply.github.com>
2022-08-16 18:04:05 +0700

    first commit

---
+++ src/components/Auth/AuthForm.js
@@ -0,0 +1,138 @@
+import { useRef, useState } from 'react';
+
+import classes from './AuthForm.module.css';
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const emailInputRef = useRef(null);
+  const passwordInputRef = useRef(null);
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    let url;
+
+    if (isLogin) {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA5gDXU2oie50gIt4DB0W5z4xfFNGhkxDw';
+    } else {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA5gDXU2oie50gIt4DB0W5z4xfFNGhkxDw';
+    }
+
+    // setIsLoading(true);
+    // fetch(url, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     email: enteredEmail,
+    //     password: enteredPassword,
+    //     returnSecureToken: true,
+    //   }),
+    // })
+    //   .then((res) => {
+    //     setIsLoading(false);
+
+    //     if (res.ok) {
+    //       return res.json();
+    //       //   .then((data) =>
+    //       //   {
+    //       //   const successMessage = `${
+    //       //     isLogin ? 'Login' : 'Sign up'
+    //       //   } successfully.`;
+    //       //   alert(successMessage);
+    //       //   console.log(data);
+    //       // });
+    //     } else {
+    //       return res.json().then((data) => {
+    //         let errorMessage = `Authenticated ${
+    //           isLogin ? 'login' : 'sign up'
+    //         } failed !`;
+
+    //         // console.log(data);
+
+    //         throw new Error(errorMessage);
+    //       });
+    //     }
+    //   })
+    //   .then((data) => {
+    //     console.log(data);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //     alert(error.message);
+    //   });
+
+    setIsLoading(true);
+    try {
+      const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+      });
+
+      const data = await res.json();
+      setIsLoading(false);
+
+      if (res.ok) {
+      } else {
+        throw new Error(data.error.message);
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor='email'>Your Email</label>
+          <input type='email' id='email' required ref={emailInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='password'>Your Password</label>
+          <input
+            type='password'
+            id='password'
+            required
+            ref={passwordInputRef}
+          />
+        </div>
+        <div className={classes.actions}>
+          {!isLoading && (
+            <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          )}
+          {isLoading && <p>Sending request...</p>}
+          <button
+            type='button'
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? 'Create new account' : 'Login with existing account'}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
